022a7985762b94dcb08ee0dd0f579ed81cd83d48
Walter Moreira <walter@waltermoreira.net>
2014-07-24 14:16:58 -0500

    Fix content-type for ping

--- sci-apps-base/python/ping.py
+++ sci-apps-base/python/ping.py
@@ -3,25 +3,32 @@
 import json
 import os
 import sys
+import datetime
 
-import intro
+import metadata
 
 import requests
 
-SERVER_URL = 'http://example.com'
+SERVER_URL = 'http://192.168.59.3:5000'
 DISALLOW_PING = '/etc/NO_PING'
+
 
 def ping_allowed():
     return not os.path.exists(DISALLOW_PING)
 
+
 def do_ping():
-    md = intro.get_metadata()
-    resp = requests.post(SERVER_URL, data=json.dumps(md))
+    md = metadata.get_metadata()
+    md['timestamp'] = datetime.datetime.now().isoformat(' ')
+    resp = requests.post(SERVER_URL, data=json.dumps(md),
+                         headers={'Content-Type': 'application/json'})
+
 
 def main():
     if ping_allowed():
         do_ping()
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
